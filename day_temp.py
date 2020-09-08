e793f5d4fa38f6d2fb275131f69bca2fd7c6ffab
HengshengGithub <hengsheng.huang@outlook.com>
2020-09-08 22:26:34 +0200

    update file for task

---
+++ day_temp.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def max_min_day(df):
+    y = 365
+    # hottest for every year
+    T_max_15 = -100
+    for j in range(0, y):
+        hottest_15_T = np.mean(df['Wert'][j*96: j*96+95])
+        if hottest_15_T > T_max_15:
+            T_max_15 = hottest_15_T
+            
+    T_max_16 = -100
+    for j in range(y, 2*y+1):
+        hottest_16_T = np.mean(df['Wert'][j*96: j*96+95])
+        if hottest_16_T > T_max_16:
+            T_max_16 = hottest_16_T
+            
+    T_max_17 = -100
+    for j in range(2*y+1, 3*y+1):
+        hottest_17_T = np.mean(df['Wert'][j*96: j*96+95])
+        if hottest_17_T > T_max_17:
+            T_max_17 = hottest_17_T
+            
+    T_max_18 = -100
+    for j in range(3*y+1, 4*y+1):
+        hottest_18_T = np.mean(df['Wert'][j*96: j*96+95])
+        if hottest_18_T > T_max_18:
+            T_max_18 = hottest_18_T
+            
+    T_max_19 = -100
+    for j in range(4*y+1, 5*y+1):
+        hottest_19_T = np.mean(df['Wert'][j*96: j*96+95])
+        if hottest_19_T > T_max_19:
+            T_max_19 = hottest_19_T
+            
+    T_max_20 = -100
+    for j in range(5*y+1, 5*y+56):
+        hottest_20_T = np.mean(df['Wert'][j*96: j*96+95])
+        if hottest_20_T > T_max_20:
+            T_max_20 = hottest_20_T
+            
+    
+    # coldest for every year
+    T_min_15 = 100
+    for j in range(0, y):
+        coldest_15_T = np.mean(df['Wert'][j*96: j*96+95])
+        if coldest_15_T < T_min_15:
+            T_min_15 = coldest_15_T
+            
+    T_min_16 = 100
+    for j in range(y, 2*y+1):
+        coldest_16_T = np.mean(df['Wert'][j*96: j*96+95])
+        if coldest_16_T < T_min_16:
+            T_min_16 = coldest_16_T
+            
+    T_min_17 = 100
+    for j in range(2*y+1, 3*y+1):
+        coldest_17_T = np.mean(df['Wert'][j*96: j*96+95])
+        if coldest_17_T < T_min_17:
+            T_min_17 = coldest_17_T
+            
+    T_min_18 = 100
+    for j in range(3*y+1, 4*y+1):
+        coldest_18_T = np.mean(df['Wert'][j*96: j*96+95])
+        if coldest_18_T < T_min_18:
+            T_min_18 = coldest_18_T
+            
+    T_min_19 = 100
+    for j in range(4*y+1, 5*y+1):
+        coldest_19_T = np.mean(df['Wert'][j*96: j*96+95])
+        if coldest_19_T < T_min_19:
+            T_min_19 = coldest_19_T
+            
+    T_min_20 = 100
+    for j in range(5*y+1, 5*y+56):
+        coldest_20_T = np.mean(df['Wert'][j*96: j*96+95])
+        if coldest_20_T < T_min_20:
+            T_min_20 = coldest_20_T
+            
+            
+    x = [2015, 2016, 2017, 2018, 2019, '2020*']
+    y1 = [T_max_15, T_max_16, T_max_17, T_max_18, T_max_19, T_max_20]
+    y2 = [T_min_15, T_min_16, T_min_17, T_min_18, T_min_19, T_min_20]
+    plt.plot(x, y1, color='r', marker='^', label='T_max')
+    plt.plot(x, y2, color='b', marker='s', label='T_min')
+    plt.legend(loc = 'center right')
+    plt.title('Temperatures for the hottest and coldest days of each year')
+    plt.text(0,-10,'*for 2020: until 25022020', size = 7)
+    plt.show()
+    
+            
+            
+
+
+
--- main.py
+++ main.py
@@ -3,33 +3,38 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from save_data import save_show_data
+from day_temp import max_min_day # import max_min_day function for plot hottest and coldest days of each year
 
 
 # laod date from file temperatures.csv
 df = pd.read_csv('temperatures.csv')
 
 
+# convert the timesteps of the temperature data to 15 min. intervals
 for k in range(0, (len(df['Wert'])*4-3), 4):
     for i in range(k+1, k+4):
         insertRow = pd.DataFrame([['TT_TU_MN009', 2014, df['Zeitstempel'][i-1]+15, np.nan, np.nan, np.nan]], columns=['Produkt_Code', 'SDO_ID', 'Zeitstempel', 'Wert', 'Qualitaet_Niveau', 'Qualitaet_Byte'])
         above = df.loc[:i-1]
         below = df.loc[i:]
         df = above.append(insertRow, ignore_index=True).append(below, ignore_index=True)
+        # Using double for-loop here, so it run a litte slowly
         
-df['Wert'].interpolate(method='linear', limit_direction='forward', axis=0, inplace=True)
-df['Qualitaet_Niveau'].interpolate(method='nearest', limit_direction='forward', axis=0, inplace=True)
-df['Qualitaet_Byte'].interpolate(method='nearest', limit_direction='forward', axis=0, inplace=True)
+df['Wert'].interpolate(method='linear', limit_direction='forward', axis=0, inplace=True)    # Using linear interpolation for temperature
+df['Qualitaet_Niveau'].interpolate(method='nearest', limit_direction='forward', axis=0, inplace=True)   # Using nearest interpolation for temperature
+df['Qualitaet_Byte'].interpolate(method='nearest', limit_direction='forward', axis=0, inplace=True)     # Using nearest interpolation for temperature
+
+df.to_csv('Temp_timesteps.csv')   # save csv file for new timesteps (with 15 min timesteps)
 
 
-
-# Find the hottest and coldest tempertures for everyear
-index_15 = df[df['Zeitstempel'].values ==201601010000].index[0]-1
+# Find the hottest and coldest tempertures for every year
+#for 2015
+index_15 = df[df['Zeitstempel'].values ==201601010000].index[0]-1       # create index
 hottest_15 = df['Wert'][:index_15].max()
 hottest_15_data = df['Zeitstempel'][df['Wert'][:index_15].idxmax()]
 coldest_15 = df['Wert'][:index_15].min()
 coldest_15_data = df['Zeitstempel'][df['Wert'][:index_15].idxmin()]
 
+#for 2016
 index_16_start = df[df['Zeitstempel'].values ==201601010000].index[0]
 index_16_end   = df[df['Zeitstempel'].values ==201701010000].index[0]-1
 hottest_16 = df['Wert'][index_16_start:index_16_end].max()
@@ -37,6 +42,7 @@ hottest_16_data = df['Zeitstempel'][df['Wert'][index_16_start:index_16_end].idxm
 coldest_16 = df['Wert'][index_16_start:index_16_end].min()
 coldest_16_data = df['Zeitstempel'][df['Wert'][index_16_start:index_16_end].idxmin()]
 
+#for 2017
 index_17_start = df[df['Zeitstempel'].values ==201701010000].index[0]
 index_17_end = df[df['Zeitstempel'].values ==201801010000].index[0]-1
 hottest_17 = df['Wert'][index_17_start:index_17_end].max()
@@ -44,6 +50,7 @@ hottest_17_data = df['Zeitstempel'][df['Wert'][index_17_start:index_17_end].idxm
 coldest_17 = df['Wert'][index_17_start:index_17_end].min()
 coldest_17_data = df['Zeitstempel'][df['Wert'][index_17_start:index_17_end].idxmin()]
 
+#for 2018
 index_18_start = df[df['Zeitstempel'].values ==201801010000].index[0]
 index_18_end = df[df['Zeitstempel'].values ==201901010000].index[0]-1
 hottest_18 = df['Wert'][index_18_start:index_18_end].max()
@@ -51,6 +58,7 @@ hottest_18_data = df['Zeitstempel'][df['Wert'][index_18_start:index_18_end].idxm
 coldest_18 = df['Wert'][index_18_start:index_18_end].min()
 coldest_18_data = df['Zeitstempel'][df['Wert'][index_18_start:index_18_end].idxmin()]
 
+#for 2019
 index_19_start = df[df['Zeitstempel'].values ==201901010000].index[0]
 index_19_end = df[df['Zeitstempel'].values ==202001010000].index[0]-1
 hottest_19 = df['Wert'][index_19_start:index_19_end].max()
@@ -58,6 +66,7 @@ hottest_19_data = df['Zeitstempel'][df['Wert'][index_19_start:index_19_end].idxm
 coldest_19 = df['Wert'][index_19_start:index_19_end].min()
 coldest_19_data = df['Zeitstempel'][df['Wert'][index_19_start:index_19_end].idxmin()]
 
+#for 2020
 index_20 = df[df['Zeitstempel'].values ==202001010000].index[0]
 hottest_20 = df['Wert'][index_20:].max()
 hottest_20_data = df['Zeitstempel'][df['Wert'][index_20:].idxmax()]
@@ -67,17 +76,53 @@ coldest_20_data = df['Zeitstempel'][df['Wert'][index_20:].idxmin()]
 
 
 # function for save csv file
-df_Temp = save_show_data()
+df_Temp = pd.DataFrame({'hottest_temp': pd.Series([hottest_15, hottest_16, hottest_17, hottest_18, hottest_19, hottest_20], 
+                                               index=[2015, 2016, 2017, 2018, 2019, 2020]),
+                     'hottest_data': pd.Series([hottest_15_data, hottest_16_data, hottest_17_data, hottest_18_data, hottest_19_data, hottest_20_data],
+                                               index=[2015, 2016, 2017, 2018, 2019, 2020]),
+                     'coldest_temp': pd.Series([coldest_15, coldest_16, coldest_17, coldest_18, coldest_19, coldest_20], 
+                                               index=[2015, 2016, 2017, 2018, 2019, 2020]),
+                     'coldest_data':pd.Series([coldest_15_data, coldest_16_data, coldest_17_data, coldest_18_data, coldest_19_data, coldest_20_data], 
+                                               index=[2015, 2016, 2017, 2018, 2019, 2020])
+                    })      # create DataFrame for save
 
-'''
-for j in range(0, 365):
-    test_15_T = np.mean(df['Wert'][0: 95])
-    T_max = -100
-    if test_15_T > T_max:
-        T_max = test_15_T
-    else:
-        continue
-'''
+df_Temp.to_csv("Temp_statistics.csv")       # save csv
+
+
+
+# plot hottest and coldest days of each year
+max_min_day(df)
+
+
+
+# one more analysis
+# average temperatur for each year
+
+mean_T_15 = np.mean(df['Wert'][:index_15])
+mean_T_16 = np.mean(df['Wert'][index_16_start:index_16_end])
+mean_T_17 = np.mean(df['Wert'][index_17_start:index_17_end])
+mean_T_18 = np.mean(df['Wert'][index_18_start:index_18_end])
+mean_T_19 = np.mean(df['Wert'][index_19_start:index_19_end])
+mean_T_20 = np.mean(df['Wert'][index_20:])
+
+#plot the average temperatur for each year
+x = [2015, 2016, 2017, 2018, 2019, '2020*']
+y = [mean_T_15, mean_T_16, mean_T_17, mean_T_18, mean_T_19, mean_T_20]
+plt.plot(x, y, marker='^')
+plt.title('average temperatur for each year')
+plt.text(0,5.8,'*for 2020: until 25022020', size = 7)
+plt.show()
+
+
+        
+        
+
+        
+
+        
+        
+
+
 
 
 
